0661442a69798e8302f549dcb31138f2a65f11fd
Philippe Schottey <schophil@icloud.com>
2021-06-27 09:12:26 +0200

    feat: Image completion

--- completions/docker.fish
+++ completions/docker.fish
@@ -18,9 +18,14 @@ function __fish_docker_all_containers
 	docker ps -a --format "{{.ID}}"
 end
 
+function __fish_docker_all_images
+	docker images
+end
+
 complete -c docker -f
 
 complete -c docker -n "not __fish_seen_subcommand_from $commands" -a "$commands"
 complete -c docker -n "__fish_seen_subcommand_from rm" -a "(__fish_docker_stopped_containers)"
 complete -c docker -n "__fish_seen_subcommand_from stop" -a "(__fish_docker_running_containers)"
+complete -c docker -n "__fish_seen_subcommand_from rmi" -a "(__fish_docker_all_images)"
 
